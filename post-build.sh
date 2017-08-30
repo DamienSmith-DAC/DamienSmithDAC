867a3b3d71568105fc713e8e43a28c2124359587
ByronAllen <ByronAllen@users.noreply.github.com>
2017-08-30 11:39:06 +1000

    Adding configs requested by Mark and more notes

--- post-build.sh
+++ post-build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
+
+# This post-build.sh script is run after the VM is built to add further configurations
+# Add file under '--name-user' flag in 'openstack create server command' 
+
+# Copy public key to root and remove root login blocking code
 cat /home/centos/.ssh/authorized_keys > /root/.ssh/authorized_keys
+
+# Umount /mnt causing it to rename as /logs
 umount /mnt
+
+# Reduce swapiness to 0
+echo 0 > /proc/sys/vm/swappiness
+
+# Disable IPv6
+echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
+echo 1 > /proc/sys/net/ipv6/conf/default/disable_ipv6 
+echo 1 > /proc/sys/net/ipv6/conf/lo/disable_ipv6 
+
+# Instantiate changes
+sysctl -p
