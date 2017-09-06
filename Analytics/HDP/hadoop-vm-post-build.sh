87b797f922bf5768b40bd0dfed897f37536c1ecc
Byron Allen <byron.a.allen@gmail.com>
2017-09-06 01:24:46 +0000

    renamed and updated

--- Analytics/HDP/hadoop-vm-post-build.sh
+++ Analytics/HDP/hadoop-vm-post-build.sh
@@ -7,8 +7,20 @@
 # Copy public key to root and remove root login blocking code
 cat /home/centos/.ssh/authorized_keys > /root/.ssh/authorized_keys
 
-# Create /hadoop/log 
+# Change hostname from hostname.gls.local to hostname.dac.local
+sed -i 's/gls/dac/g' /etc/hostname
+sed -i 's/gs/dac/g' /etc/hostname
+sed -i 's/- set_hostname/#- set_hostname/g' /etc/cloud/cloud.cfg
+sed -i 's/- update_hostname/#- update_hostname/g' /etc/cloud/cloud.cfg
+
+# Mount /hadoop/log 
 mkdir -p /hadoop/log
+umount /mnt
+umount /logs
+sed -i '/^\/dev\/vdb/d' /etc/fstab
+mkfs.ext4 /dev/vdb
+echo "/dev/vdb /hadoop/log ext4 defaults 0 2" >> /etc/fstab
+mount /dev/vdb /hadoop/log
 
 # Reduce swapiness to 0
 echo 0 > /proc/sys/vm/swappiness
