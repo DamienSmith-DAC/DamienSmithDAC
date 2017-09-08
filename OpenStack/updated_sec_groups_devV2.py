e21fe4c2edf352e0967c824b3b57e81e3a2045b3
timothyheyes <30921704+timothyheyes@users.noreply.github.com>
2017-09-08 17:03:50 +1000

    Update updated_sec_groups_devV2.py

--- OpenStack/updated_sec_groups_devV2.py
+++ OpenStack/updated_sec_groups_devV2.py
@@ -8,8 +8,8 @@ viz_security_group= {
                       'description': "'Allow all HTTP and HTTPS in, as well as comms across the viz_security_group on all ports.",
                       'rules':
                         [
-						  ['tcp', 'ingress', '-1', '-1', '', "'viz_security_group'"],
-						  ['tcp', 'ingress', '80', '80', '0.0.0.0/0', "''"],
+			  ['tcp', 'ingress', '-1', '-1', '', "'viz_security_group'"],
+			  ['tcp', 'ingress', '80', '80', '0.0.0.0/0', "''"],
                           ['tcp', 'ingress', '443' , '443', '0.0.0.0/0', "''"]
                         ]
                     }
@@ -21,13 +21,13 @@ dp_security_group= {
                       'description': "'Allow mysql and sql-server from the Vizualisation subnet'",
                       'rules':
                         [
-						  ['tcp', 'ingress', '-1', '-1', "''", 'dp_security_group'],
+			  ['tcp', 'ingress', '-1', '-1', "''", 'dp_security_group'],
                           ['tcp', 'ingress', '3306', '3306', "''", 'viz_security_group'],
                           ['tcp', 'ingress', '1433', '1433', "''", 'viz_security_group'],
-						  ['tcp', 'ingress', '3306', '3306', "''", 'analytics_security_group'] #as discussed
-						  ['tcp', 'ingress', '1433', '1433', "''", 'analytics_security_group'] #allow analytics_security_group to push data to dataproducts layer
-						  ['tcp', 'ingress', '1433', '1433', 'openvpn', "''"]
-						  ['tcp', 'ingress', '3306', '3306', 'openvpn', "''"]		        
+			  ['tcp', 'ingress', '3306', '3306', "''", 'analytics_security_group'] #as discussed
+			  ['tcp', 'ingress', '1433', '1433', "''", 'analytics_security_group'] #allow analytics_security_group to push data to dataproducts layer
+			  ['tcp', 'ingress', '1433', '1433', 'openvpn', "''"]
+			  ['tcp', 'ingress', '3306', '3306', 'openvpn', "''"]		        
                         ]
                     }
 
@@ -38,7 +38,7 @@ edge_security_group= {
                       'rules':
                         [
                           ['tcp', 'ingress', '8787', '8787', 'openvpn', "''"],
-						  ['tcp', 'ingress', '-1', '-1', '', "'edge_security_group'"],
+			  ['tcp', 'ingress', '-1', '-1', '', "'edge_security_group'"],
                           ['tcp', 'ingress', '9995', '9995', 'openvpn', "''"],
                           ['tcp', 'ingress', '8889', '8889', 'openvpn', "''"]
                         ]
@@ -50,24 +50,24 @@ analytics_security_group= {
                       'description': "'Allow Ambari, Hive Metastore, Hive Web UI, Hive Server, Spark, Spark History Server from trusted VPN machines. Allow connections from the edge node subnet from ports 1019 to 50475'",
                       'rules':
                         [
-						  ['tcp', 'ingress', '-1', '-1', '', "'analytics_security_group'"],
-						  ['tcp', 'ingress', '1019', '50475', '', "'edge_security_group'"], #allow imbound connections from edge node subnet
+			  ['tcp', 'ingress', '-1', '-1', '', "'analytics_security_group'"],
+			  ['tcp', 'ingress', '1019', '50475', '', "'edge_security_group'"], #allow imbound connections from edge node subnet
                           ['tcp', 'ingress', '3000', '3000', 'openvpn', "''"],
                           ['tcp', 'ingress', '6080', '6080', 'openvpn', "''"],
                           ['tcp', 'ingress', '8080', '8080', 'openvpn', "''"],
                           ['tcp', 'ingress', '8088', '8088', 'openvpn', "''"],
-						  ['tcp', 'ingress', '8886', '8886', 'openvpn', "''"],
-						  ['tcp', 'ingress', '16010', '16010', 'openvpn', "''"],
-						  ['tcp', 'ingress', '18081', '18081', 'openvpn', "''"],
-						  ['tcp', 'ingress', '19888', '19888', 'openvpn', "''"],
-						  ['tcp', 'ingress', '21000', '21000', 'openvpn', "''"],
-						  ['tcp', 'ingress', '50070', '50070', 'openvpn', "''"],
+			  ['tcp', 'ingress', '8886', '8886', 'openvpn', "''"],
+			  ['tcp', 'ingress', '16010', '16010', 'openvpn', "''"],
+			  ['tcp', 'ingress', '18081', '18081', 'openvpn', "''"],
+			  ['tcp', 'ingress', '19888', '19888', 'openvpn', "''"],
+			  ['tcp', 'ingress', '21000', '21000', 'openvpn', "''"],
+			  ['tcp', 'ingress', '50070', '50070', 'openvpn', "''"],
                           ['tcp', 'ingress', '18080', '18080', 'openvpn', "''"]
 						  
-						  #Not clear on which ports these are exactly but I'm assuming its the 
-						  #hive/ambari/spark ports? Different to the original ports in this script. 
+			  #Not clear on which ports these are exactly but I'm assuming its the 
+			  #hive/ambari/spark ports? Different to the original ports in this script. 
 						  
-						  #will get steve to send a snapshot of current rules for verification
+			 #will get steve to send a snapshot of current rules for verification
                         ]
                     }
 					
@@ -77,14 +77,14 @@ landing_zone_security_group= {
                       'description': "'Allow SSH, HTTPS from the analytics_security_group. Allow intergroup comms and all HTTPS traffic'",
                       'rules':
                         [
-						  ['tcp', 'ingress', '22', '22', '', "'analytics_security_group'"],
-						  ['tcp', 'ingress', '443', '443', '', "'analytics_security_group'"],
-						  ['tcp', 'ingress', '-1', '-1', '', "'landing_zone_security_group'"],
-						  ['tcp', 'ingress', '443', '443', '0.0.0.0/0', "''"]   
+			  ['tcp', 'ingress', '22', '22', '', "'analytics_security_group'"],
+			  ['tcp', 'ingress', '443', '443', '', "'analytics_security_group'"],
+			  ['tcp', 'ingress', '-1', '-1', '', "'landing_zone_security_group'"],
+			  ['tcp', 'ingress', '443', '443', '0.0.0.0/0', "''"]   
 						  
-						  #Allows all inbound HTTPS traffic, as discussed with Byron (should HTTP be included too?). 
-						  #Cloudflare handles IP whitelisting *this still seems insecure because in the 
-						  #latest pentest Cloudflare could be circumvented if you knew the server IP
+			  #Allows all inbound HTTPS traffic, as discussed with Byron (should HTTP be included too?). 
+			  #Cloudflare handles IP whitelisting *this still seems insecure because in the 
+			  #latest pentest Cloudflare could be circumvented if you knew the server IP
                         ]
                     }	
 					
@@ -95,8 +95,8 @@ default_security_group= {
                       'rules':
                         [
                           ['tcp', 'egress', '-1', '-1', '0.0.0.0/0', "''"],
-						  ['tcp', 'ingress', '22', '22', 'openvpn', "''"],
-						  ['tcp', 'ingress', '3389', '3389', 'openvpn', "''"]
+			  ['tcp', 'ingress', '22', '22', 'openvpn', "''"],
+			  ['tcp', 'ingress', '3389', '3389', 'openvpn', "''"]
                         ]
                     }
 					
