23f951bfb0db63b69db69129b2988f2b1daa7ae8
Byron Allen <byron.a.allen@gmail.com>
2017-09-20 05:51:21 +0000

    update mysql build script and add rstudio server install script

---
+++ Analytics/RStudioServer/install_rstudio_server.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install RStudio Server (freemium version) to CentOS 7
+
+yum -y install epel-release
+yum -y install R R-devel libcurl-devel openssl-devel 
+yum -y update R
+R -e "print(1+1)" #test if R is running
+yum -y remove epel-release
+wget https://download2.rstudio.org/rstudio-server-rhel-1.0.143-x86_64.rpm
+yum -y install --nogpgcheck rstudio-server-rhel-1.0.143-x86_64.rpm
+yum -y remove epel-release
--- MySQL/mysql-server-build.sh
+++ MySQL/mysql-server-build.sh
@@ -61,18 +61,18 @@ echo "/dev/vdb /db ext4 defaults 0 2" >> /etc/fstab
 mount /dev/vdb /db
 
 # Install MySQL Cluster
-sleep 120
-wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.5/mysql-cluster-community-7.5.7-1.el7.x86_64.rpm-bundle.tar
-tar xvf mysql-cluster-community-7.5.7-1.el7.x86_64.rpm-bundle.tar
-yum -y install perl-Data-Dumper
-yum -y remove mariadb-libs-5.5.52-1.el7.x86_64
-yum -y install mysql-cluster-community-common-7.5.7-1.el7.x86_64.rpm
-yum -y install mysql-cluster-community-libs-7.5.7-1.el7.x86_64.rpm
+#sleep 120
+#wget https://dev.mysql.com/get/Downloads/MySQL-Cluster-7.5/mysql-cluster-community-7.5.7-1.el7.x86_64.rpm-bundle.tar
+#tar xvf mysql-cluster-community-7.5.7-1.el7.x86_64.rpm-bundle.tar
+#yum -y install perl-Data-Dumper
+#yum -y remove mariadb-libs-5.5.52-1.el7.x86_64
+#yum -y install mysql-cluster-community-common-7.5.7-1.el7.x86_64.rpm
+#yum -y install mysql-cluster-community-libs-7.5.7-1.el7.x86_64.rpm
 
 # Check for latest package of perl-Class-MethodMaker
-yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/p/perl-Class-MethodMaker-2.20-1.el7.x86_64.rpm
-yum -y install mysql-cluster-community-client-7.5.7-1.el7.x86_64.rpm
-yum -y install mysql-cluster-community-server-7.5.7-1.el7.x86_64.rpm
+#yum -y install http://dl.fedoraproject.org/pub/epel/7/x86_64/p/perl-Class-MethodMaker-2.20-1.el7.x86_64.rpm
+#yum -y install mysql-cluster-community-client-7.5.7-1.el7.x86_64.rpm
+#yum -y install mysql-cluster-community-server-7.5.7-1.el7.x86_64.rpm
 
 # Configure cluster
 #cd /db
