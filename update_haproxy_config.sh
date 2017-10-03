c30a05e15b57572d89241d84b2a80f15643ab10b
Chris Soulier <christopher.soulier@servian.com>
2017-10-04 09:11:08 +1100

    corrected some bugs from adding branch selector

--- update_haproxy_config.sh
+++ update_haproxy_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #execute from p-dachaproxy-21
-
+CUR_DIR=`pwd`
 #SET GITHUB VARIABLES
 GIT_OWNER="DFSI-DAC"
 GIT_REPO="Platform"
@@ -9,26 +9,27 @@ GIT_BRANCH="master"
 GIT_FILE="https://api.github.com/repos/$GIT_OWNER/$GIT_REPO/contents/$GIT_PATH?ref=$GIT_BRANCH"
 
 #Backup existing haproxy.cfg on both HAPROXY servers
-cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.`date +%Y%m%d` 
-ssh p-dachaproxy-22 "cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.`date +%Y%m%d`"
+cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.`date +%Y%m%d-%H%M%S`
+ssh p-dachaproxy-22 "cp /etc/haproxy/haproxy.cfg /etc/haproxy/haproxy.cfg.`date +%Y%m%d-%H%M%S`"
 cd /tmp
 
 echo "Please paste your personal git token"
 echo "For instructions on how to create a token visit;"
 echo "https://help.github.com/articles/creating-a-personal-access-token-for-the-command-line/"
-read GIT_TOKEN
+read -p "Please paste your personal git token: "  GIT_TOKEN
 
 curl --header "Authorization: token $GIT_TOKEN" \
     --header "Accept: application/vnd.github.v3.raw" \
-    --remote-name \
-    --location $GIT_FILE
+    --location $GIT_FILE \
+    -o /tmp/haproxy.cfg
 
 unset GIT_TOKEN
 
 #Check for syntax validity of new haproxy.cfg 
 
-if [ "`haproxy -f /tmp/haproxy.cfg -c 2>/dev/null`" == "Configuration file is valid" ]
+if [ "`haproxy -f /tmp/haproxy.cfg -c 2>/dev/null`" = "Configuration file is valid" ]
   then
+   echo 'Config file correct'
     scp /tmp/haproxy.cfg p-dachaproxy-22:/etc/haproxy/haproxy.cfg
     mv -f /tmp/haproxy.cfg /etc/haproxy/haproxy.cfg
     service haproxy reload
@@ -38,3 +39,4 @@ if [ "`haproxy -f /tmp/haproxy.cfg -c 2>/dev/null`" == "Configuration file is va
     rm -f /tmp/haproxy.cfg
     exit
 fi
+cd $CUR_DIR
