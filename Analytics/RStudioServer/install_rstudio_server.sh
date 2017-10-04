431f902e37a6d537af6a952c58876ddb4c518606
Byron Allen <byron.a.allen@gmail.com>
2017-10-04 05:06:03 +0000

    minor edit

--- Analytics/RStudioServer/install_rstudio_server.sh
+++ Analytics/RStudioServer/install_rstudio_server.sh
@@ -6,7 +6,7 @@ yum -y install epel-release
 yum -y install R R-devel libcurl-devel openssl-devel 
 yum -y update R
 R -e "print(1+1)" #test if R is running
-yum -y remove epel-release
+yum -y install epel-release
 wget https://download2.rstudio.org/rstudio-server-rhel-1.0.153-x86_64.rpm
 yum -y install --nogpgcheck rstudio-server-rhel-1.0.153-x86_64.rpm
 yum -y remove epel-release
