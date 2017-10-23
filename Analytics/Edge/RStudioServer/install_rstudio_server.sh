a7751eb78880710bff146b818b1bb4554f8944c4
Byron Allen <byron.allen@servian.com>
2017-10-24 09:55:08 +1100

    update install script with details about installing pro edition

--- Analytics/Edge/RStudioServer/install_rstudio_server.sh
+++ Analytics/Edge/RStudioServer/install_rstudio_server.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Install RStudio Server (freemium version) to CentOS 7
+# Script used to install RStudio Server on CentOS 7
 
+# Setup
 yum -y install epel-release
-yum -y install R R-devel libcurl-devel openssl-devel 
+yum -y install R R-devel libcurl-devel openssl-devel
 yum -y update R
 R -e "print(1+1)" #test if R is running
-yum -y install epel-release
-wget https://download2.rstudio.org/rstudio-server-rhel-1.0.153-x86_64.rpm
-yum -y install --nogpgcheck rstudio-server-rhel-1.0.153-x86_64.rpm
+
+# Install free, open-source version
+#wget https://download2.rstudio.org/rstudio-server-rhel-1.0.153-x86_64.rpm
+#yum -y install --nogpgcheck rstudio-server-rhel-1.0.153-x86_64.rpm
+
+# Install enterprise edition
+wget https://download2.rstudio.org/rstudio-server-rhel-pro-1.0.153-x86_64.rpm
+yum -y install --nogpgcheck rstudio-server-rhel-pro-1.0.153-x86_64.rpm
 yum -y remove epel-release
