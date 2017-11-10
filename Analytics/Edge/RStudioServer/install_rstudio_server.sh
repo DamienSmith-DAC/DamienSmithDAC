5d3cc839a43e8b1584d13dcf0051d44cc6de491b
ByronAllen <ByronAllen@users.noreply.github.com>
2017-11-10 12:20:53 +1100

    advance automation of RStudio Server Pro instal

--- Analytics/Edge/RStudioServer/install_rstudio_server.sh
+++ Analytics/Edge/RStudioServer/install_rstudio_server.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Script used to install RStudio Server on CentOS 7
+# This script requires active inputs (i.e. the license key and installation of sparklyr)
 
 # Setup
 yum -y install epel-release
@@ -8,22 +9,62 @@ yum -y install R R-devel libcurl-devel openssl-devel
 yum -y update R
 R -e "print(1+1)" #test if R is running
 
+
 # Install free, open-source version
-#wget https://download2.rstudio.org/rstudio-server-rhel-1.0.153-x86_64.rpm
-#yum -y install --nogpgcheck rstudio-server-rhel-1.0.153-x86_64.rpm
+# should you choose this option you will not need the below
+# sans some of the configs (possibly)
+# wget https://download2.rstudio.org/rstudio-server-rhel-1.0.153-x86_64.rpm
+# yum -y install --nogpgcheck rstudio-server-rhel-1.0.153-x86_64.rpm
+
 
 # Install enterprise edition
 wget https://download2.rstudio.org/rstudio-server-rhel-pro-1.0.153-x86_64.rpm
 yum -y install --nogpgcheck rstudio-server-rhel-pro-1.0.153-x86_64.rpm
 yum -y remove epel-release
 
+
+# Activate RStudio Server Pro 
+OLDIFS=$IFS
+echo "Please enter your RStudio Server Pro license key followed by Ctrl + D:"
+IFS="   "
+PEM=`cat`
+echo $PEM > /rstudio_license_key
+IFS=$OLDIFS
+rstudio-server license-manager activate < /rstudio_license_key
+rstudio-server restart
+
+
 # Install requirement for Kerberos
 yum -y install pam_krb5
 
+
 # Config files
+cat >> /etc/rstudio/rserver.conf <<EOF
+auth-stay-signed-in-days=7
+auth-pam-sessions-profile=rstudio-session
+auth-pam-sessions-use-password=1
+auth-pam-sessions-close=1
+EOF
+
+cat >> /etc/pam.d/rstudio <<EOF
+auth       sufficient     pam_krb5.so debug
+account    required       pam_krb5.so debug
+session    requisite      pam_krb5.so debug
+EOF
+
+cat >> /etc/pam.d/rstudio-session <<EOF
+auth        required      pam_krb5.so debug
+auth        optional      pam_mount.so use_first_pass
+account     [default=bad success=ok user_unknown=ignore] pam_krb5.so debug
+password    sufficient    pam_krb5.so use_authtok debug
+session     requisite     pam_krb5.so debug
+EOF
+
 echo "readenv=1" >> /etc/environment
+
 
 # Install sparklyr (execute manually to select CRAN mirror)
 yum -y install libxml2-devel
 echo "Install sparklyr with following command (execute manually to select CRAN mirror)"
 echo "R -e \"install.packages('sparklyr')\""
+
