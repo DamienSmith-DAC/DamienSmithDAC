be7a2eadb2f9affc69725498afeb34feb940f7f7
timothyheyes <30921704+timothyheyes@users.noreply.github.com>
2017-11-13 14:41:09 +1100

    Create V2-PROD-hdfs-home-dir-and-ldap-sync.sh

---
+++ Migration_to_v2.0/Users/Create_HDFS_home_directories_and_LDAP_sync/V2-PROD-hdfs-home-dir-and-ldap-sync.sh
@@ -0,0 +1,48 @@
+#PROD SCRIPT
+#This script sync's ldap with ambari server, then creates a home directory for any new users synced through ldap. Output can be found at /var/log/onboardingautomatino/create-hdfs-home-dir-and-ldap-sync.log
+
+LDAP_PASSWORD="/etc/onboardingautomation/ldapsync.conf"
+EXISTING_HDFS_USERS="/etc/onboardingautomation/existing-hdfs-users.txt"
+USER_DIFFERENCES="/etc/onboardingautomation/user-differences.txt"
+
+MYSQL_PASSWORD_FILE="/etc/onboardingautomation/sqlpassword.conf" #This is stored on the mysql server
+SQLQUERY='use ambari;select user_name from users;'
+SQL_USER_LIST="/etc/onboardingautomation/sql-user-list.txt"
+MYSQL_PASSWORD_FILE='/etc/onboardingautomation/sqlpassword.conf'
+MYSQL_PASSWORD=`ssh a-mysql-02 "cat $MYSQL_PASSWORD_FILE"`
+
+#NOTE A-MYSQL-02 IS THE PRIMARY DB, AND LOGIN IS HIVE. SQL PASSWORD STORED ON SQL SERVER
+
+exec 2>> /var/log/onboardingautomation/create-hdfs-home-dir-and-ldap-sync.log
+
+ssh a-mysql-02 'mysql -u hive -p'"'"$MYSQL_PASSWORD"'"' -N -e '"'"$SQLQUERY"'" > $SQL_USER_LIST
+
+ambari-server sync-ldap --groups /etc/ambari-server/conf/groups.txt --ldap-sync-admin-name=admin --ldap-sync-admin-password=$(< "$LDAP_PASSWORD")
+if [ $? -ne 0 ]; then
+        exit
+fi
+
+kinit hdfs-dace2@DAC.LOCAL -kt /etc/security/keytabs/hdfs.headless.keytab
+if [ $? -ne 0 ]; then
+        exit
+fi
+
+hdfs dfs -ls /user | cut -d '/' -f 3 | tail -n +2 > $EXISTING_HDFS_USERS
+if [ $? -ne 0 ]; then
+        exit
+fi
+
+grep -Fxvf $EXISTING_HDFS_USERS $SQL_USER_LIST > $USER_DIFFERENCES
+if [ $? -ne 0 ]; then
+        exit
+fi
+
+while read LDAPUSER;
+        do
+                        hdfs dfs -mkdir /user/$LDAPUSER
+                        hdfs dfs -chown $LDAPUSER:hdfs /user/$LDAPUSER
+        done < $USER_DIFFERENCES
+
+if [ $? -ne 0 ]; then
+        exit
+fi
