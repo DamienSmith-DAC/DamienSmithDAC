06f9060799e7b9b91bbcc853dd54577b34beee45
timothyheyes <30921704+timothyheyes@users.noreply.github.com>
2017-11-13 14:47:27 +1100

    Create V2-ambari-edge-ldap-sync.sh

---
+++ Migration_to_v2.0/Users/Create_HDFS_home_directories_and_LDAP_sync/V2-ambari-edge-ldap-sync.sh
@@ -0,0 +1,10 @@
+#This script syncs the ambari server with ldap. It uses the password stored in /etc/onboardingautomation/ldapsync.conf
+
+LDAP_PASSWORD="/etc/onboardingautomation/ldapsync.conf"
+
+exec 2>> /var/log/ldapsync/ldapsync.log
+
+ambari-server sync-ldap --groups /etc/ambari-server/conf/groups.txt --ldap-sync-admin-name=admin --ldap-sync-admin-password=$(< "$LDAP_PASSWORD")
+if [ $? -ne 0 ]; then
+        exit
+fi
