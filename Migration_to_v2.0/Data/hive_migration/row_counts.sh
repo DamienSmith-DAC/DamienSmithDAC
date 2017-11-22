bb6c06ec40ac029ee755492e7067e0e590106cd5
ByronAllen <ByronAllen@users.noreply.github.com>
2017-11-22 11:30:21 +1100

    adding logging

--- Migration_to_v2.0/Data/hive_migration/row_counts.sh
+++ Migration_to_v2.0/Data/hive_migration/row_counts.sh
@@ -4,7 +4,7 @@
 # Run this script in both environments and compare the difference between the outputs
 
 databases=`hive -e "show databases;"`
-path_to_dbs=/tmp/hive_migration/databases/
+base_path=/tmp/hive_migration
 
 for db in $databases;
 
@@ -16,3 +16,22 @@ for db in $databases;
 				echo ${db}_${t}_count_`hive -e "use ${db}; select count(*) from ${t};"` >> row_counts 
 			done
 	done
+
+#Logging
+PROCESS_LOG_PATH=${base_path}/logs
+function log_info()
+{
+log_time=date "+%Y-%m-%d:%H:%M:%S"
+echo -e "$* $log_time" >> ${PROCESS_LOG_PATH}
+}
+log_info "Info: Process started at"
+
+#This code to check for any failure at any step
+if [ $? -eq 0 ];then
+log_info "Info : Command Completed"
+else
+log_info "Error : Command Failed"
+exit 1
+fi
+
+log_info "Info: Process completed at"
