6dfd9c41b9bb8cd6efe68687abfd6b131323f080
ByronAllen <ByronAllen@users.noreply.github.com>
2017-12-01 14:24:12 +1100

    updating code for prod

--- Cleanup/usercleanup.sh
+++ Cleanup/usercleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Script to remove users that have been marked as inactive
-# Script is located in the production external metastore server
+# Script is located in the production external metastore server number 2
 
 BASE_DIR=/etc/usercleanup
 MYSQL_PASSWORD=`cat /etc/onboardingautomation/sqlpassword.conf` 
@@ -14,12 +14,12 @@ echo "$(date +'%h %d %H:%M:%S')"
 
 # Backup Ambari Database
 echo 'Backing up Ambari database...'
-mysqldump -u root -d ambari -p${MYSQL_PASSWORD} > ${BASE_DIR}/ambari.sql.backup
+mysqldump -u hive -d ambariviews -p${MYSQL_PASSWORD} > ${BASE_DIR}/ambariviews.sql.backup
 
 
 # Find Inactive Users and Remove User Directories
 echo 'Creating users_to_delete.txt...'
-mysql -u root -D ambari -p${MYSQL_PASSWORD} -e "select user_id from users where active = 0;" > ${USER_TO_DELETE}
+mysql -u hive -D ambariviews -p${MYSQL_PASSWORD} -e "select user_id from users where active = 0;" > ${USER_TO_DELETE}
 
 
 # If file is not zero size
@@ -34,13 +34,13 @@ if [ -s ${USER_TO_DELETE} ]; then
 			then
 			
 			# Find User Name
-			USER_NAME_TMP=`mysql -u root -D ambari -p${MYSQL_PASSWORD} -e "select user_name from users where user_id = ${USER_ID}"`
+			USER_NAME_TMP=`mysql -u hive -D ambariviews -p${MYSQL_PASSWORD} -e "select user_name from users where user_id = ${USER_ID}"`
 			PREFIX="user_name "
 			USER_NAME=`echo $USER_NAME_TMP | sed -e "s/$PREFIX//"`
 			echo Removing ${USER_NAME} directories...
 
 			# Remove HDFS Directory 
-			ssh -n np-master02.dac.local "kinit hdfs-npdace2@DAC.LOCAL -kt /etc/security/keytabs/hdfs.headless.keytab; hdfs dfs -rm -r /user/${USER_NAME}" 
+			ssh -n np-master02.dac.local "kinit hdfs-dace2@DAC.LOCAL -kt /etc/security/keytabs/hdfs.headless.keytab; hdfs dfs -rm -r /user/${USER_NAME}" 
 			
 			# Remove Edge Node(s) Directories 
 			ssh -n np-edge.dac.local "cd /home; rm -rfv ${USER_NAME};"
@@ -56,6 +56,6 @@ if [ -s ${USER_TO_DELETE} ]; then
 
 	# Remove Inactive Users from Ambari Database
 	echo 'Removing inactive user(s) from Ambari database...'
-	mysql -u root -D ambari -p${MYSQL_PASSWORD} -e "delete from members where user_id in (select user_id from users where active = 0); delete from users where active = 0;"
+	mysql -u hive -D ambariviews -p${MYSQL_PASSWORD} -e "delete from members where user_id in (select user_id from users where active = 0); delete from users where active = 0;"
 
 fi
