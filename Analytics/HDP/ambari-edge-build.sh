0a43d101800695b044395435696b08fd20786a23
ByronAllen <ByronAllen@users.noreply.github.com>
2017-12-08 23:53:58 +1100

    removed SSSD and update various parts

--- Analytics/HDP/ambari-edge-build.sh
+++ Analytics/HDP/ambari-edge-build.sh
@@ -6,26 +6,37 @@
 # Pre-flight checklist
 	# Executed and follow instructions in ambari-agent-build.sh
 
-# In the edge node
+# In the target edge node, copy over certs, keytabs, and kerb config 
+# from master or other ambari edge
 mkdir /etc/security/serverKeys
-mkdir /etc/security/keytabs
-
-# From Ambari Server Master, transfer truststore, certs, the Ambari Keytab, and kerb config
-scp -r /etc/security/serverKeys root@np-ambari-edge-test:/etc/security/
-scp /etc/security/keytabs/ambari.server.keytab root@np-ambari-edge-test:/etc/security/keytabs/
-scp /etc/krb5.conf root@np-ambari-edge-test:/etc/
-
-# In the edge node
+scp -r root@ambari-edge-01:/etc/security/serverKeys /etc/security/ 
+scp root@ambari-edge-01:/etc/security/keytabs/ambari.server.keytab /etc/security/keytabs/
+scp root@ambari-edge-01:/etc/krb5.conf /etc/ 
 chmod 755 /etc/security/serverKeys/
 chmod 444 /etc/security/serverKeys/*
+chown -R yarn:hadoop /etc/security/serverKeys/
 
 # Install Ambari Server on the edge node
 yum -y install ambari-server
 yum -y install mysql-connector-java
 
 # In the external metastore server 
-mysql -u ambari -p -e "create database ambariviews01;"
-mysql -u ambari -p -D ambariviews01 < Ambari-DDL-MySQL-CREATE.sql
+mysql -u ambariviews0x -p -e "create database ambariviews01;"
+
+# Create new credentials for ambariviews0x
+# Follow this guide https://github.com/DFSI-DAC/Platform/blob/master/MySQL/install-external-mysql-service.sh
+# For example:
+	# CREATE USER 'ambariviews02'@'%' IDENTIFIED BY 'password';
+	# GRANT ALL privileges on *.* to 'ambariviews02'@'%' WITH GRANT OPTION;
+	# CREATE USER 'ambariviews02'@'localhost' IDENTIFIED BY 'password';
+	# GRANT ALL privileges on *.* to 'ambariviews02'@'localhost' WITH GRANT OPTION;
+	# CREATE USER 'ambariviews02'@'ambari-edge-02.dac.local' IDENTIFIED BY 'password';
+	# GRANT ALL privileges on *.* to 'ambariviews02'@'ambari-edge-02.dac.local' WITH GRANT OPTION;
+	# CREATE USER 'ambariviews02'@'master02.dac.local' IDENTIFIED BY 'password';
+	# GRANT ALL privileges on *.* to 'ambariviews02'@'master02.dac.local' WITH GRANT OPTION;
+	# FLUSH PRIVILEGES;
+
+mysql -u ambariviews0x -p -D ambariviews0x < Ambari-DDL-MySQL-CREATE.sql
 
 # Execute the setup command
 ambari-server setup 
@@ -38,20 +49,17 @@ ambari-server setup
 ambari-server setup-security
 # Select and configure [3] Setup Ambari kerberos JAAS configuration (use ambari-server-dace2@DAC.LOCAL)
 # Select and configure [4] Setup truststore using /etc/security/serverKeys/all.jks 
-# If using HTTPS, select and confiugre [1] Enable HTTPS for Ambari server
-
-# Enable SSSD
-./enable_sssd.sh
+# If using HTTPS, select and confiugre [1] Enable HTTPS for Ambari server (note, testing this has failed for ambari-edge-02)
 
 # Go to your 
 # Start Ambari Server 
 ambari-server start
 
 # Sync AD and new Ambari Edge
-scp ambari-edge-02:/etc/ambari-server/conf/groups.txt /etc/ambari-server/conf/ 
+scp ambari-edge-01:/etc/ambari-server/conf/groups.txt /etc/ambari-server/conf/ 
 ambari-server setup-ldap # See /etc/ambari-server/conf/ambari.properties in exiting Ambari Server or Edge Node for more details
 ambari-server restart
-/usr/sbin/ambari-server sync-ldap --groups /etc/ambari-server/conf/groups.txt 
+ambari-server sync-ldap --groups /etc/ambari-server/conf/groups.txt 
 
 # Go to http://<AmbariEdgeIP>:8080/views/ADMIN_VIEW/2.5.2.0/INSTANCE/#/remoteClusters
 # Register the 'remote' cluster
