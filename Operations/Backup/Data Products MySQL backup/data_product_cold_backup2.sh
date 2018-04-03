e9feb66df8e14f061ef6f5d6a392c0eb995720b4
neelakai <indu.neelakandan@treasury.nsw.gov.au>
2018-04-03 11:42:23 +1000

    Remove copying incremental backup to cold backup; delete incremental backup

--- Operations/Backup/Data Products MySQL backup/data_product_cold_backup2.sh
+++ Operations/Backup/Data Products MySQL backup/data_product_cold_backup2.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 # is a script for cold backup from data products
-# from /dataproducts/full to /analytics/dataproducts_cold_backup/full and
-# from /dataproducts/incr to /analytics/dataproducts_cold_backup/incr
-# It'll copy a full set of files from previous saturday to friday
+# from /dataproducts/full to /analytics/dataproducts_cold_backup/full 
+# Deletes incremental backup files for the previous 6 days.
+# It'll copy the full backup file from previous Saturday
 # Cronjob for this Script will run on Friday 18:00 UTC
 ###############################################################################
-# version 1.0
+# version 1.1
 # Developer Chetna S
+###############################################################################
+# Change history:
+# Version 1.1: Removed code that copies incremental backup to
+#              dataproducts_cold_backup/incr
 ###############################################################################
 
 #variables for full
@@ -35,48 +39,10 @@ function log_info()
 RSYNC="rsync -a"
 
 #start logging
-log_info "Info: Start copy of incremental backup "
-
-# create local directory if it doesnt exit
-[ ! -d ${destination_local_dir_incr} ] && mkdir -p ${destination_local_dir_incr}
-
-if [ $? -eq 0 ]; then
-  log_info "Info: Created directory ${destination_local_dir_incr}"
-else
-  log_info "Info: Local directory ${destination_local_dir_incr} exists"
-fi
-
-# Copy last 7 days file (incremental backup)
-ok=0
-for i in {0..5}; do
-    source_local_dir_incr=$(date -d "$(date) - $i day" +"%Y/%m/%d")
-    $RSYNC "${source_parent_dir_incr}/${source_local_dir_incr}"  "${destination_local_dir_incr}";
-    if [ $? -eq 0 ]; then
-        log_info "Info: Copy Completed for ${source_parent_dir_incr}/${source_local_dir_incr}"
-        rm -rf "${source_parent_dir_incr}/${source_local_dir_incr}";
-        if [ $? -eq 0 ]; then
-            log_info "Info: Deletion Complete for ${source_parent_dir_incr}/${source_local_dir_incr}"
-        else
-            log_info "Info: Error in Deleting ${source_parent_dir_incr}/${source_local_dir_incr}"
-            ok=1
-        fi
-    else
-       log_info "Info: Error in copy incremenatal backup ${source_parent_dir_incr}/${source_local_dir_incr}"
-       ok=1
-   fi
-done
-
-if [ $ok -eq 0 ]; then
-    log_info "Info: Incremental Backup Copy Completed"
-else
-    log_info "Info: Error in Incremental Backup Copy"
-fi
-
-#start logging
 log_info "Info: Start copy of full backup"
 
- # create local directory if it doesnt exit
-  [ ! -d ${destination_local_dir_full} ] && mkdir -p ${destination_local_dir_full}
+# create local directory if it doesnt exit
+[ ! -d ${destination_local_dir_full} ] && mkdir -p ${destination_local_dir_full}
 
 if [ $? -eq 0 ]; then
    log_info "Info: Created directory ${destination_local_dir_full}"
@@ -87,22 +53,45 @@ fi
 # Copy previous Saturday's file
 OK=0
 $RSYNC "${weekly_source_dir_full}"  "${destination_local_dir_full}";
-    if [ $? -eq 0 ]; then
-        log_info "Info: Copy Completed for ${weekly_source_dir_full}"
-        rm -rf "${weekly_source_dir_full}";
-        if [ $? -eq 0 ]; then
-             log_info "Info: Deletion Complete for ${weekly_source_dir_full}"
-        else
-             log_info "Info: Error in Deleting ${weekly_source_dir_full}"
-             OK=1
-        fi
-   else
-       log_info "Info:Error in copying Full Backup ${weekly_source_dir_full}"
-       OK=1
 
-   fi
+if [ $? -eq 0 ]; then
+	log_info "Info: Copy Completed for ${weekly_source_dir_full}"
+	rm -rf "${weekly_source_dir_full}";
+	if [ $? -eq 0 ]; then
+		 log_info "Info: Deletion Complete for ${weekly_source_dir_full}"
+	else
+		 log_info "Info: Error in Deleting ${weekly_source_dir_full}"
+		 OK=1
+	fi
+else
+   log_info "Info:Error in copying Full Backup ${weekly_source_dir_full}"
+   OK=1
+
+fi
+
 if [ $OK -eq 0 ]; then
     log_info "Info: Full Backup Copy Completed"
 else
     log_info "Info: Error in Full Backup Copy"
 fi
+
+#start logging
+log_info "Info: Start cleanup of incremental backup "
+
+# Delete last 6 days of incremental backup
+ok=0
+for i in {0..5}; do
+
+    source_local_dir_incr=$(date -d "$(date) - $i day" +"%Y/%m/%d")
+    
+    rm -rf "${source_parent_dir_incr}/${source_local_dir_incr}";
+    if [ $? -eq 0 ]; then
+		log_info "Info: Deletion Complete for ${source_parent_dir_incr}/${source_local_dir_incr}"
+    else
+        log_info "Info: Error in Deleting ${source_parent_dir_incr}/${source_local_dir_incr}"
+        ok=1
+    fi
+ 
+done
+
+log_info "Info: Cleanup of incremental backup completed "
