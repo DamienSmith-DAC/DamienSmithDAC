f680b64bc93b8a9db37ff23b21d08d61ba559834
timothyheyes <30921704+timothyheyes@users.noreply.github.com>
2018-04-09 12:24:45 +1000

    Update ssh_access_groups.sh
    fixed to allow dace2_non_prod_external, dace2_non_prod_internal, root

--- Security/np_ssh_access/ssh_access_groups.sh
+++ Security/np_ssh_access/ssh_access_groups.sh
@@ -2,6 +2,6 @@
 #NP Edge node is not included in datamaster_nodes.txt
 #Users in dace2_non_prod_external and internal cannot access the data and master nodes
 
-pssh -i -h datamaster_nodes.txt 'sed -i "/UsePAM yes/a DenyGroups dace2_non_prod_external dace2_non_prod_internal" /etc/ssh/sshd_config'
+pssh -i -h datamaster_nodes.txt 'sed -i "/UsePAM yes/a AllowGroups dace2_non_prod_external dace2_non_prod_internal root" /etc/ssh/sshd_config'
 sleep 5
 pssh -i -h datamaster_nodes.txt service sshd restart
