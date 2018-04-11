29fcfca3e7fc25efa4250a13a2466823c72116fa
neelakai <indu.neelakandan@treasury.nsw.gov.au>
2018-04-11 16:37:59 +1000

    Qlik backup scripts

---
+++ Operations/Backup/Visualisation backup/Viz Qlik backup/web_node_Qlik_site_backup.ps1
@@ -0,0 +1,63 @@
+﻿# Set local variables and paths
+$Today                 = Get-Date -UFormat "%Y%m%d"
+$StartTime             = Get-Date -UFormat "%Y%m%d_%H%M"
+ 
+$BackupTargetLocation  = "D:\qlik_backup"
+$LogFileLocation       =  Join-Path -ChildPath $StartTime -Path $BackupTargetLocation
+$LogFileName           =  Join-Path -ChildPath "$StartTime.log" -Path $LogFileLocation 
+
+ 
+# Program Data Folder
+$SenseProgramData = "C:\ProgramData"
+
+# Create today's backup directory
+
+md $BackupTargetLocation\$StartTime
+
+$TodaysTargetLocation  =  Join-Path -ChildPath $StartTime -Path $BackupTargetLocation
+
+"#########################################################################################" | Out-File -FilePath $LogFileName -Append
+"Backup Day is $Today" | Out-File -FilePath $LogFileName -Append
+"Backup Start time is $StartTime" |  Out-File -FilePath $LogFileName -Append
+"BackupTargetLocation is $BackupTargetLocation" | Out-File -FilePath $LogFileName -Append
+"TodaysTargetLocation is $TodaysTargetLocation" | Out-File -FilePath $LogFileName -Append
+"#########################################################################################" | Out-File -FilePath $LogFileName -Append
+
+
+"Stopping Qlik Services ...." | Out-File -FilePath $LogFileName -Append
+
+stop-service QlikSenseProxyService -WarningAction SilentlyContinue
+stop-service QlikSenseEngineService -WarningAction SilentlyContinue
+stop-service QlikSenseSchedulerService -WarningAction SilentlyContinue
+stop-service QlikSensePrintingService -WarningAction SilentlyContinue
+stop-service QlikSenseServiceDispatcher -WarningAction SilentlyContinue
+stop-service QlikLoggingService -WarningAction SilentlyContinue
+stop-service QlikSenseRepositoryService -WarningAction SilentlyContinue
+ 
+
+"Backing up Program Data from $SenseProgramData ...." | Out-File -FilePath $LogFileName -Append
+
+Copy-Item  $SenseProgramData\Qlik -Destination $TodaysTargetLocation\ProgramData\Qlik -Recurse
+
+"Backing up Program Data is Completed" | Out-File -FilePath $LogFileName -Append
+
+"File Backup Completed." | Out-File -FilePath $LogFileName -Append
+
+ 
+"Restarting Qlik Services ...." | Out-File -FilePath $LogFileName -Append
+
+start-service QlikSenseRepositoryService -WarningAction SilentlyContinue
+start-service QlikSenseEngineService -WarningAction SilentlyContinue
+start-service QlikSenseSchedulerService -WarningAction SilentlyContinue
+start-service QlikSensePrintingService -WarningAction SilentlyContinue
+start-service QlikSenseServiceDispatcher -WarningAction SilentlyContinue
+start-service QlikSenseProxyService -WarningAction SilentlyContinue
+start-service QlikLoggingService -WarningAction SilentlyContinue
+
+
+"Qlik Services restarted...." | Out-File -FilePath $LogFileName -Append
+
+
+$EndTime = Get-Date -UFormat "%Y%m%d_%H%M%S"
+ 
+"This backup process started on $env:computername at  $StartTime  and ended at $EndTime. " | Out-File -FilePath $LogFileName -Append
