81f4bc683acb3b55d7c290829d36197541d51351
timothyheyes <timothy.heyes@student.uts.edu.au>
2018-06-05 13:18:09 +1000

    updating script to include logging and actual backup location. Added comments on future use at the top.

--- Operations/Backup/Swift Backup/swift_backup.sh
+++ Operations/Backup/Swift Backup/swift_backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#to backup another directory, mimic the structure of hdfs/hive backups and simply add that directory to the backup_list.txt file in /root/backup.d/
 #########################################################################################
 # 											#
 #	This script compresses and encrypts backups under /analytics/ then		#
@@ -9,18 +10,22 @@
 
 
 DATE=$(date +"%Y_%m_%d")
-ZIPCOUNT=0 #This is a counter used to track the amount of retries for zipping the data.
-SENDCOUNT=0 #This is a counter used to track the amount of retries for sending the data to openstack.
-ENCRYPTCOUNT=0 #This is a counter used to track the amount of retries for encrypting the data.
+LOG_PATH="/analytics/log/SWIFT_$(date +%a%Y%m%d).log"
+
+# Log function
+function log_info()
+{
+    log_time=`date "+%Y-%m-%d:%H:%M:%S"`
+    echo -e "$* $log_time" >> ${LOG_PATH}
+}
 
 while read BACKUP; do
 	
 	if [ "$BACKUP" == "dataproducts_cold_backup" ]; then
-		#find /analytics/dataproducts_cold_backup/full/ -mindepth 3 -type d -mtime +14 -mtime -28 > ${DATE}_backup_directory_list.txt #same as below for dp.
-		find /root/testdir/encrypt/dp/full/ -mindepth 3 -type d -mtime +14 -mtime -28 > ${DATE}_backup_directory_list.txt 
-		echo "dp found, backing it up..."
+		find /analytics/dataproducts_cold_backup/full/ -mindepth 3 -type d -mtime +14 -mtime -28 > ${DATE}_backup_directory_list.txt #same as below for dp.
+		#find /root/testdir/encrypt/dp/full/ -mindepth 3 -type d -mtime +14 -mtime -28 > ${DATE}_backup_directory_list.txt 
 	else
-		find /root/testdir/encrypt/$BACKUP/ -mindepth 1 -maxdepth 1 -type d -mtime +14 -mtime -28 > ${DATE}_backup_directory_list.txt #create list all 2+week old folders
+		find /analytics/$BACKUP/ -mindepth 1 -maxdepth 1 -type d -mtime +14 -mtime -28 > ${DATE}_backup_directory_list.txt #create list all 2+week old folders
 	fi
 
 	while read FOLDER; do #loops through all the directories in $DATE_backup_directory_list.txt	
@@ -29,90 +34,54 @@ while read BACKUP; do
 		ls $FOLDER > $BASEFILE.list #creating list with same name of backup.tar which hold all files being backed up in that tar.
 		tar -czf $BASEFILE.tar.gz -C $FOLDER . #compress all the files inside $FOLDER
 	
-		while [ "$?" -ne 0 -a "$ZIPCOUNT" -le 4 ]; do #check if previous command was successful
-			echo "zipping failed $ZIPCOUNT time(s). Retrying..."
-			ZIPCOUNT=`expr $ZIPCOUNT +1`
-			tar -czf $BASEFILE.tar.gz -C $FOLDER . #retry compress all the files inside $FOLDER
-		done	
+		if [ "$?" -ne 0 ]; then #check if previous command was successful
+			log_info "Error: zipping of $BASEFILE.tar.gz failed."
+			exit 1
+		fi	
 	
-		if [ "$ZIPCOUNT" -ge 4 ]; then
-			echo "zipping of $FOLDER failed. Exiting script."
-                	exit 1
-		else
-			echo "$FOLDER successfully zipped."
-                	ZIPCOUNT=0
-		fi
+		log_info  "Info: $FOLDER successfully zipped."
 		
-		echo -e "encrypting $BASEFILE.tar.gz here using aes256...\n"
-		#openssl enc -aes256 -in $BASEFILE.tar.gz -out $BASEFILE.tar.gz.enc #encrypt the zipped folder.
+		echo -e "encrypting $BASEFILE.tar.gz using aes256...\n"
 		openssl enc -aes256 -in $BASEFILE.tar.gz -out $BASEFILE.tar.gz.enc -pass file:/root/.backup.d/pass.key
 
-		while [ "$?" -ne 0 -a "$ENCRYPTCOUNT" -le 4 ]; do #check if ecrypting was sucessful.
-                	echo "encrypting failed $ENCRYPTCOUNT time(s). Retrying..."
-                	ENCRYPTCOUNT=`expr $ENCRYPTCOUNT +1`
-			#openssl enc -aes256 -in $BASEFILE.tar.gz -out $BASEFILE.tar.gz.enc #retry encrypt the zipped folder.
-			openssl enc -aes256 -in $BASEFILE.tar.gz -out $BASEFILE.tar.gz.enc -pass file:/root/.backup.d/pass.key
-		done
+		if [ "$?" -ne 0 ]; then #check if ecrypting was successful.
+                	log_info "Error: encrypting of $BASEFILE.tar.gz failed."
+			exit 1
+		fi
 
-        	if [ "$ENCRYPTCOUNT" -ge 4 ]; then
-                	echo "encrypting of $BASEFILE.tar.gz failed. Exiting script."
-                	exit 1
-        	else
-                	echo "$BASEFILE.tar.gz successfully encrypted. Removing $BASEFILE.tar.gz locally."
-                	ENCRYPTCOUNT=0
-			rm -f $BASEFILE.tar.gz #remove local zip file after it has been encrypted.
-        	fi
+                log_info "Info: $BASEFILE.tar.gz successfully encrypted. Removing $BASEFILE.tar.gz locally."
+		rm -f $BASEFILE.tar.gz #remove local zip file after it has been encrypted.
         	
-                echo -e "Sending backup to swift container. \n"
+                echo -e "Sending $BASEFILE.tar.gz.enc to swift container. \n"
 		
 		if [ "$BACKUP" == "dataproducts_cold_backup" ]; then
 			DP="$(echo $FOLDER | sed 's/^.//')"
 			openstack --insecure object create DAC_BACKUP/$DP $BASEFILE.tar.gz.enc #sends zipped, encrypted file to openstack container.
 		else
-			echo "sending backup to swift..."
 			openstack --insecure object create DAC_BACKUP/analytics/$BACKUP $BASEFILE.tar.gz.enc #sends zipped, encrypted file to openstack container.
 		fi
 
-		while [ "$?" -ne 0 -a "$SENDCOUNT" -le 4 ]; do
-			echo "sending backup to swift failed $SENDCOUNT time(s). Retrying..."
-			SENDCOUNT=`expr $SENDCOUNT + 1`
-			if [ "$BACKUP" == "dataproducts_cold_backup" ]; then
-				echo "sending backup to swift failed $SENDCOUNT time(s). Retrying..."
-				openstack --insecure object create DAC_BACKUP/$DP $BASEFILE.tar.gz.enc #sends zipped, encrypted file to openstack container.
-                        else
-        	        	echo "sending backup to swift failed $SENDCOUNT time(s). Retrying..."
-				openstack --insecure object create DAC_BACKUP/analytics/$BACKUP $BASEFILE.tar.gz.enc #sends zipped, encrypted file to openstack container.
-                	fi
-		done
-
-		if [ "$SENDCOUNT" -ge 4 ]; then #exits script if sending of data fails 4 times, else resets counter for next file.
-			echo "sending of $BASEFILE.tar.gz.enc to swift failed. Exiting script."
+		if [ "$?" -ne 0 ]; then
+			log_info "Error: sending $BASEFILE.tar.gz.enc to DAC_BACKUP failed."
 			exit 1
-		else
-			echo -e "Sending list file to swift container. \n"
-			if [ "$BACKUP" == "dataproducts_cold_backup" ]; then
-				openstack --insecure object create DAC_BACKUP/$DP $BASEFILE.list #sends zipped, encrypted file to openstack container.
-                        else
-                                openstack --insecure object create DAC_BACKUP/analytics/$BACKUP $BASEFILE.list #sends zipped, encrypted file to openstack container.
-                        fi
 		fi
-			while [ "$?" -ne 0 -a "$SENDCOUNT" -le 4 ]; do
-                        	echo "sending list to swift failed $SENDCOUNT time(s). Retrying..."
-                        	SENDCOUNT=`expr $SENDCOUNT + 1`
-                        	if [ "$BACKUP" == "dataproducts_cold_backup" ]; then
-                                	echo "sending list to swift failed $SENDCOUNT time(s). Retrying..."
-					openstack --insecure object create DAC_BACKUP/$DP $BASEFILE.list #sends zipped, encrypted file to openstack container.
-                        	else
-                                	echo "sending list to swift failed $SENDCOUNT time(s). Retrying..."
-					openstack --insecure object create DAC_BACKUP/analytics/$BACKUP $BASEFILE.list #sends zipped, encrypted file to openstack container.
-                       		fi
+	
+		log_info "Info: $BASEFILE.tar.gz.enc sent to DAC_BACKUP. Removing $BASEFILE.tar.gz.enc locally."
+		rm -f $BASEFILE.tar.gz.enc #delete encrypted file after it has been uploaded to swift container.
 
-	                done
+		if [ "$BACKUP" == "dataproducts_cold_backup" ]; then
+			openstack --insecure object create DAC_BACKUP/$DP $BASEFILE.list #sends zipped, encrypted file to openstack container.
+                else
+                        openstack --insecure object create DAC_BACKUP/analytics/$BACKUP $BASEFILE.list #sends zipped, encrypted file to openstack container.
+                fi
+		
+		if [ "$?" -ne 0 ]; then
+                        log_info "Error: sending $BASEFILE.list to DAC_BACKUP failed."
+                        exit 1
+		fi
 
-			echo "$BASEFILE.tar.gz.enc successfully sent to swift container. Removing $BASEFILE.tar.gz.enc and list file locally."
-			SENDCOUNT=0
-			rm -f $BASEFILE.tar.gz.enc #delete encrypted file after it has been uploaded to swift container.
-			rm -f $BASEFILE.list
+		log_info "Info: $BASEFILE.list sent to DAC_BACKUP. Removing $BASEFILE.list locally."
+		rm -f $BASEFILE.list
 
 	done < ${DATE}_backup_directory_list.txt
 
