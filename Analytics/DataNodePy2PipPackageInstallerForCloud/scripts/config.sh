42c0d5aa44e5fa711a438b6ced2be70fb0beed02
Ali Baig <ali.baig@treasury.nsw.gov.au>
2019-02-18 00:26:06 +0000

    changed file name

--- Analytics/DataNodePy2PipPackageInstallerForCloud/scripts/config.sh
+++ Analytics/DataNodePy2PipPackageInstallerForCloud/scripts/config.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 _THIS_DIR="$(cd "$(dirname "$0")"; pwd)"
-HOST_FILE="${_THIS_DIR}/../host_lists/dace2_data_node_addresses.txt"
+HOST_FILE="${_THIS_DIR}/../host_lists/dace2_node_addresses.txt"
