3c7f306088fd3a52c085f5cc9620ecb7ce2abffd
Ali Baig <ali.baig@treasury.nsw.gov.au>
2019-02-18 00:48:45 +0000

    changed pssh command to avoid connection errors

--- Analytics/DataNodePy2PipPackageInstallerForCloud/scripts/run_import_test.sh
+++ Analytics/DataNodePy2PipPackageInstallerForCloud/scripts/run_import_test.sh
@@ -7,10 +7,9 @@ THIS_DIR="$(cd "$(dirname "$0")"; pwd)"
 source "${THIS_DIR}/config.sh"
 
 TEST_SCRIPT="${THIS_DIR}/../test/import_test.sh"
-#HOST_FILE="${THIS_DIR}/../host_lists/dace2_data_node_addresses.txt"
 STDOUT_DIR="${THIS_DIR}/../logs/import_test_pssh_out"
 STDERR_DIR="${THIS_DIR}/../logs/import_test_pssh_err"
 # 5 mins
 TIMEOUT=300
 
-pssh -h "${HOST_FILE}" -t "${TIMEOUT}" -o "${STDOUT_DIR}" -e "${STDERR_DIR}" -Pv -I < "${TEST_SCRIPT}"
+pssh -O StrictHostKeyChecking=no -h "${HOST_FILE}" -t "${TIMEOUT}" -o "${STDOUT_DIR}" -e "${STDERR_DIR}" -Pv -I < "${TEST_SCRIPT}"
